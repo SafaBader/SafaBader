f6d758f1d882a11a2eff8b873bc5f0ee95a04d28
Safa Bader <safa.bader28@hotmail.com>
2025-12-01 20:31:18 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+const firstName = "Safa";
+const lastName = "Bader";
+const birthYear = 1999;
+let currentYear = 2025;
+let isEmployed = false;
+let salary;
+let bestFriend = null;
+let age = currentYear - birthYear;
+const fullName = firstName + lastName;
+
+console.log(`My Full Name is ${fullName}`);
+console.log(`My Age is ${age}`);
+console.log(` Employment status is ${isEmployed}`);
+console.log(`salary ${salary}`);
+console.log(`My best friend ${bestFriend}`);
+
+console.log(`type of Full Name is ${typeof (fullName)}`);
+console.log(`type of Age is ${typeof (age)}`);
+console.log(` type of Employment status is ${typeof (isEmployed)}`);
+console.log(`type of salary ${typeof (salary)}`);
+console.log(`type of My best friend ${typeof (bestFriend)}`);
+
+console.log(`my name is ${fullName} and I am ${age} years old`);
+
+
+
+
+
